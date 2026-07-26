2cf0cdb34a5ffdbb1d4750e4afd921767ad51657
Felipe Sepulveda <32464471+felisep@users.noreply.github.com>
2026-07-26 20:15:09 +0200

    added countdown

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,3 +1,36 @@
-export default function Countdown() {
-	return <h1>Here is the countdown</h1>;
+import { intervalToDuration } from "date-fns/intervalToDuration";
+import { useEffect, useState } from "react";
+
+type CountdownProps = {
+  birthDate: Date;
+};
+
+export default function Countdown({ birthDate }: CountdownProps) {
+  const targetDate = new Date(birthDate);
+  targetDate.setFullYear(targetDate.getFullYear() + 18);
+
+  const [now, setNow] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setNow(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const duration = intervalToDuration({
+    start: now,
+    end: targetDate,
+  });
+
+  return (
+    <div>
+      {duration.years} years{" "}
+      {duration.months} months{" "}
+      {duration.days} days{" "}
+      {duration.hours} hours{" "}
+      {duration.minutes} minutes{" "}
+    </div>
+  );
 }
